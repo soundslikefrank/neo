c1f23f7ea5d17c1e4ad21c49dd2bee8219b281db
Christian Maniewski <code@chmanie.com>
2021-08-23 15:15:24 +0200

    Switch to Arduino 33 BLE

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,52 +1,29 @@
-// Basic demo for accelerometer, gyro, and magnetometer readings
-// from the following Adafruit ST Sensor combo boards:
-// * LSM6DSOX + LIS3MDL FeatherWing : https://www.adafruit.com/product/4565
-// * ISM330DHCX + LIS3MDL FeatherWing https://www.adafruit.com/product/4569
-// * LSM6DSOX + LIS3MDL Breakout : https://www.adafruit.com/product/4517
-// * LSM6DS33 + LIS3MDL Breakout Lhttps://www.adafruit.com/product/4485
-
-#include <Adafruit_LSM6DSOX.h>
-Adafruit_LSM6DSOX lsm6ds;
-
-// To use with the LSM6DS33+LIS3MDL breakout, uncomment these two lines
-// and comment out the lines referring to the LSM6DSOX above
-//#include <Adafruit_LSM6DS33.h>
-//Adafruit_LSM6DS33 lsm6ds;
-
-// To use with the ISM330DHCX+LIS3MDL Feather Wing, uncomment these two lines
-// and comment out the lines referring to the LSM6DSOX above
-//#include <Adafruit_ISM330DHCX.h>
-//Adafruit_ISM330DHCX lsm6ds;
-
-#include <Adafruit_LIS3MDL.h>
-Adafruit_LIS3MDL lis3mdl;
-
 #include <BLEMIDI_Transport.h>
-
-#include <hardware/BLEMIDI_ESP32_NimBLE.h>
-/* #include <hardware/BLEMIDI_ESP32.h> */
-// #include <hardware/BLEMIDI_nRF52.h>
-// #include <hardware/BLEMIDI_ArduinoBLE.h>
+#include <hardware/BLEMIDI_ArduinoBLE.h>
 
 BLEMIDI_CREATE_INSTANCE("GLOVE", MIDI)
 
 unsigned long t0 = millis();
 bool isConnected = false;
+int fingers[5] = {0, 0, 0, 0, 0};
 
 // -----------------------------------------------------------------------------
-// When BLE connected, LED will turn on (indication that connection was
-// successful) When receiving a NoteOn, LED will go out, on NoteOff, light comes
-// back on. This is an easy and conveniant way to show that the connection is
-// alive and working.
+// When BLE connected, LED will turn on (indication that connection was successful)
+// When receiving a NoteOn, LED will go out, on NoteOff, light comes back on.
+// This is an easy and conveniant way to show that the connection is alive and working. 
 // -----------------------------------------------------------------------------
-void setup() {
+void setup()
+{
   MIDI.begin();
   Serial.begin(115200);
 
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
-
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+
   digitalWrite(LED_BUILTIN, LOW);
 
   BLEMIDI.setHandleConnected([]() {
@@ -65,301 +42,58 @@ void setup() {
   MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity) {
     digitalWrite(LED_BUILTIN, HIGH);
   });
-  delay(1000);
-
-  Serial.println("Adafruit LSM6DS+LIS3MDL test!");
-
-  bool lsm6ds_success, lis3mdl_success;
-
-  // hardware I2C mode, can pass in address & alt Wire
-
-  lsm6ds_success = lsm6ds.begin_I2C();
-  lis3mdl_success = lis3mdl.begin_I2C();
-
-  if (!lsm6ds_success) {
-    Serial.println("Failed to find LSM6DS chip");
-  }
-  if (!lis3mdl_success) {
-    Serial.println("Failed to find LIS3MDL chip");
-  }
-  if (!(lsm6ds_success && lis3mdl_success)) {
-    while (1) {
-      delay(10);
-    }
-  }
-
-  Serial.println("LSM6DS and LIS3MDL Found!");
-
-  // lsm6ds.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (lsm6ds.getAccelRange()) {
-  case LSM6DS_ACCEL_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case LSM6DS_ACCEL_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case LSM6DS_ACCEL_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case LSM6DS_ACCEL_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  // lsm6ds.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Accelerometer data rate set to: ");
-  switch (lsm6ds.getAccelDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
-
-  // lsm6ds.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
-  Serial.print("Gyro range set to: ");
-  switch (lsm6ds.getGyroRange()) {
-  case LSM6DS_GYRO_RANGE_125_DPS:
-    Serial.println("125 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_250_DPS:
-    Serial.println("250 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_500_DPS:
-    Serial.println("500 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_1000_DPS:
-    Serial.println("1000 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_2000_DPS:
-    Serial.println("2000 degrees/s");
-    break;
-  case ISM330DHCX_GYRO_RANGE_4000_DPS:
-    Serial.println("4000 degrees/s");
-    break;
-  }
-  // lsm6ds.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Gyro data rate set to: ");
-  switch (lsm6ds.getGyroDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
-
-  lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
-  // You can check the datarate by looking at the frequency of the DRDY pin
-  Serial.print("Magnetometer data rate set to: ");
-  switch (lis3mdl.getDataRate()) {
-  case LIS3MDL_DATARATE_0_625_HZ:
-    Serial.println("0.625 Hz");
-    break;
-  case LIS3MDL_DATARATE_1_25_HZ:
-    Serial.println("1.25 Hz");
-    break;
-  case LIS3MDL_DATARATE_2_5_HZ:
-    Serial.println("2.5 Hz");
-    break;
-  case LIS3MDL_DATARATE_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  case LIS3MDL_DATARATE_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case LIS3MDL_DATARATE_20_HZ:
-    Serial.println("20 Hz");
-    break;
-  case LIS3MDL_DATARATE_40_HZ:
-    Serial.println("40 Hz");
-    break;
-  case LIS3MDL_DATARATE_80_HZ:
-    Serial.println("80 Hz");
-    break;
-  case LIS3MDL_DATARATE_155_HZ:
-    Serial.println("155 Hz");
-    break;
-  case LIS3MDL_DATARATE_300_HZ:
-    Serial.println("300 Hz");
-    break;
-  case LIS3MDL_DATARATE_560_HZ:
-    Serial.println("560 Hz");
-    break;
-  case LIS3MDL_DATARATE_1000_HZ:
-    Serial.println("1000 Hz");
-    break;
-  }
-
-  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
-  Serial.print("Range set to: ");
-  switch (lis3mdl.getRange()) {
-  case LIS3MDL_RANGE_4_GAUSS:
-    Serial.println("+-4 gauss");
-    break;
-  case LIS3MDL_RANGE_8_GAUSS:
-    Serial.println("+-8 gauss");
-    break;
-  case LIS3MDL_RANGE_12_GAUSS:
-    Serial.println("+-12 gauss");
-    break;
-  case LIS3MDL_RANGE_16_GAUSS:
-    Serial.println("+-16 gauss");
-    break;
-  }
-
-  lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE);
-  Serial.print("Magnetometer performance mode set to: ");
-  switch (lis3mdl.getPerformanceMode()) {
-  case LIS3MDL_LOWPOWERMODE:
-    Serial.println("Low");
-    break;
-  case LIS3MDL_MEDIUMMODE:
-    Serial.println("Medium");
-    break;
-  case LIS3MDL_HIGHMODE:
-    Serial.println("High");
-    break;
-  case LIS3MDL_ULTRAHIGHMODE:
-    Serial.println("Ultra-High");
-    break;
-  }
-
-  lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
-  Serial.print("Magnetometer operation mode set to: ");
-  // Single shot mode will complete conversion and go into power down
-  switch (lis3mdl.getOperationMode()) {
-  case LIS3MDL_CONTINUOUSMODE:
-    Serial.println("Continuous");
-    break;
-  case LIS3MDL_SINGLEMODE:
-    Serial.println("Single mode");
-    break;
-  case LIS3MDL_POWERDOWNMODE:
-    Serial.println("Power-down");
-    break;
-  }
-
-  lis3mdl.setIntThreshold(500);
-  lis3mdl.configInterrupt(false, false, true, // enable z axis
-                          true,               // polarity
-                          false,              // don't latch
-                          true);              // enabled!
 }
 
-// -----------------------------------------------------------------------------
-//
-// -----------------------------------------------------------------------------
-
-int potValue = 0;
-int value = 0;
-
-void loop() {
+void loop()
+{
   MIDI.read();
 
-  if (millis() - t0 > 1000) {
+  if (isConnected && (millis() - t0) > 1000)
+  {
     t0 = millis();
-    potValue = analogRead(34);
-    value = map(potValue, 2840, 4000, 0, 127);
-    value = constrain(value, 0, 127);
-    Serial.println(value);
-    if (isConnected) {
-      /* MIDI.sendControlChacge(1, value, 15); */
-      MIDI.sendNoteOn(60, 100, 1); // note 60, velocity 100 on channel 1
-    }
 
-    sensors_event_t accel, gyro, mag, temp;
+    MIDI.sendNoteOn (60, 100, 1); // note 60, velocity 100 on channel 1
 
-    //  /* Get new normalized sensor events */
-    lsm6ds.getEvent(&accel, &gyro, &temp);
-    lis3mdl.getEvent(&mag);
+    fingers[0] = analogRead(A0); // thumb
+    fingers[1] = analogRead(A1); // index finger
+    fingers[2] = analogRead(A2); // middle finger
+    fingers[3] = analogRead(A3); // ring finger
+    fingers[4] = analogRead(A4); // little finger
 
-    /* Display the results (acceleration is measured in m/s^2) */
-    Serial.print("\t\tAccel X: ");
-    Serial.print(accel.acceleration.x, 4);
-    Serial.print(" \tY: ");
-    Serial.print(accel.acceleration.y, 4);
-    Serial.print(" \tZ: ");
-    Serial.print(accel.acceleration.z, 4);
-    Serial.println(" \tm/s^2 ");
+    /* fingers[0] = map(fingers[0], 2840, 4000, 0, 127);
+    fingers[0] = constrain(fingers[0], 0, 127);
 
-    /* Display the results (rotation is measured in rad/s) */
-    Serial.print("\t\tGyro  X: ");
-    Serial.print(gyro.gyro.x, 4);
-    Serial.print(" \tY: ");
-    Serial.print(gyro.gyro.y, 4);
-    Serial.print(" \tZ: ");
-    Serial.print(gyro.gyro.z, 4);
-    Serial.println(" \tradians/s ");
+    fingers[1] = map(fingers[1], 2840, 4000, 0, 127);
+    fingers[1] = constrain(fingers[1], 0, 127);
 
-    /* Display the results (magnetic field is measured in uTesla) */
-    Serial.print(" \t\tMag   X: ");
-    Serial.print(mag.magnetic.x, 4);
-    Serial.print(" \tY: ");
-    Serial.print(mag.magnetic.y, 4);
-    Serial.print(" \tZ: ");
-    Serial.print(mag.magnetic.z, 4);
-    Serial.println(" \tuTesla ");
+    fingers[2] = map(fingers[2], 2840, 4000, 0, 127);
+    fingers[2] = constrain(fingers[2], 0, 127);
 
-    Serial.print("\t\tTemp   :\t\t\t\t\t");
-    Serial.print(temp.temperature);
-    Serial.println(" \tdeg C");
-    Serial.println();
+    fingers[3] = map(fingers[3], 2840, 4000, 0, 127);
+    fingers[3] = constrain(fingers[3], 0, 127);
+
+    fingers[4] = map(fingers[4], 2840, 4000, 0, 127);
+    fingers[4] = constrain(fingers[4], 0, 127); */
+
+    Serial.print("thumb: ");
+    Serial.println(fingers[0]);
+
+    Serial.print("index finger: ");
+    Serial.println(fingers[1]);
+
+    Serial.print("middle finger: ");
+    Serial.println(fingers[2]);
+
+    Serial.print("ring finger: ");
+    Serial.println(fingers[3]);
+
+    Serial.print("little finger: ");
+    Serial.println(fingers[4]);
   }
 }
+
+
+
+
+
+
